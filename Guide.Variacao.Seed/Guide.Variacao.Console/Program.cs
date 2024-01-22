18267ad6967304b58531b091ed28915cdec6c201
Humberto Filho <hleitefilho@hotmail.com>
2024-01-22 01:31:25 -0300

    Configurando propiamente o "Readme" e adicionando o projeto de testes para termos certeza que a comunicação com a API esta ok!

--- Guide.Variacao.Seed/Guide.Variacao.Console/Program.cs
+++ Guide.Variacao.Seed/Guide.Variacao.Console/Program.cs
@@ -9,19 +9,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Creating services");
         var builder = Host.CreateApplicationBuilder(args)
             .Configure();
 
         var host = builder.Build();
         var serviceProvide = host.Services;
 
+        Console.WriteLine("Ensuring that the database is created");
         //Ensure that the Database is properly created
         serviceProvide.Reset<GuideVariacaoContext>();
 
         var StockService = serviceProvide.GetMyService<IStockService>();
 
+        Console.WriteLine("Retrieving data from the Yahoo Finance API");
         var stocks = StockService.GetFromAPI().Result;
 
+        Console.WriteLine("Addind data to the database...");
         StockService.Add(stocks);
+
+        Console.WriteLine("Done configuring!");
     }
 }
